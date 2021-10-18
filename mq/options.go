652772aa539001c91372547ff2ec0c89140d8150
Jarod <xinghen249@gmail.com>
2021-10-18 16:57:48 +0800

    update options.go - add WithTopic Option function to config subscribe only subscribe some topic or set message topic - add WithTimeout to config subscribe receive message timeout

--- mq/options.go
+++ mq/options.go
@@ -1,7 +1,28 @@
 package mq
 
+import "time"
+
 // Options is used to extend for subscribe method
 type Options map[string]interface{}
 
 // Option is callback function to edit Options
 type Option func(opts *Options)
+
+const (
+	TopicOptionKey   = "topic"   // the topic key name in options
+	TimeoutOptionKey = "timeout" // the timeout key name in options
+)
+
+// WithTopic is set message topic or set subscriber only subscribe some topic
+func WithTopic(topic string) Option {
+	return func(opts *Options) {
+		(*opts)[TopicOptionKey] = topic
+	}
+}
+
+// WithTimeout is set subscriber receive message timeout
+func WithTimeout(timeout time.Duration) Option {
+	return func(opts *Options) {
+		(*opts)[TimeoutOptionKey] = timeout
+	}
+}
